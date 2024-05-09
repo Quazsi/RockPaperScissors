07f5514f6a83d5cf18758ee9ebf442718f25af7b
Adam Leveille <114101515+Quazsi@users.noreply.github.com>
2024-05-09 02:26:18 -0300

    Add a play round function
    The play round function gets the player's and
    computer's choices and then determines the winner.

--- rps.js
+++ rps.js
@@ -1,16 +1,14 @@
 
-let humanScore = 0;
-let computerScore = 0;
 // Randomly return rock, paper, or scissors 
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random() * 3)
 
     if (randomNumber == 0){
-        return "Rock"
+        return "rock"
     } else if (randomNumber == 1) {
-        return "Paper"
+        return "paper"
     } else if (randomNumber == 2) {
-        return "Scissors"
+        return "scissors"
     }
 
 }
@@ -19,10 +17,67 @@ console.log(getComputerChoice())
 //Prompt user for their choice
 function getHumanChoice(){
    let choice = prompt("Rock, Paper, or Scissors?")
-   return choice
+   return choice.toLowerCase();
 }
 
-console.log(getHumanChoice())
-function playRound(){
 
+function playRound(){
+    //Get the computers choice
+    let computersChoice = getComputerChoice()
+    //Get the players choice
+    let humansChoice = getHumanChoice()
+    //Determine who won
+    if (humansChoice == "rock") {
+        switch(computersChoice){
+            case "rock":
+                console.log("It's a Tie!")
+                break;
+            case "paper":
+                console.log("You lose!")
+                computerScore++ 
+                break;
+            case "scissors":
+                console.log("You win!")
+                humanScore++
+                break;
+        }
+    }
+    if (humansChoice == "paper") {
+        switch(computersChoice){
+            case "rock":
+                console.log("You lose!") 
+                break;
+            case "paper":
+                console.log("It's a Tie!") 
+                break;
+            case "scissors":
+                console.log("You win!")
+                humanScore++
+
+                break;
+        }
+    }
+    if (humansChoice == "scissors") {
+        switch(computersChoice){
+            case "rock":
+                console.log("You lose!")
+                computerScore++  
+                break;
+            
+            case "paper": 
+                console.log("You win!")
+                humanScore++
+                break;
+            
+            case "scissors":
+                console.log("It's a Tie!") 
+                break;
+        }
+    }
+}
+
+function playGame() {
+
+    let humanScore = 0;
+    let computerScore = 0;
 }
