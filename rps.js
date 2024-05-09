489db36d8e2de5617b27a80e5325c5ad0e2c1766
Adam Leveille <114101515+Quazsi@users.noreply.github.com>
2024-05-09 01:42:28 -0300

    Add getComputerChoice function
    The function selects and returns a random choice
    of Rock, paper, or scissors to be used as the
    computers choice.

---
+++ rps.js
@@ -0,0 +1,26 @@
+
+let humanScore = 0;
+let computerScore = 0;
+// Randomly return rock, paper, or scissors 
+function getComputerChoice(){
+    let randomNumber = Math.floor(Math.random() * 3)
+
+    if (randomNumber == 0){
+        return "Rock"
+    } else if (randomNumber == 1) {
+        return "Paper"
+    } else if (randomNumber == 2) {
+        return "Scissors"
+    }
+
+}
+console.log(getComputerChoice())
+
+//Prompt user for their choice
+function getHumanChoice(){
+
+}
+
+function playRound(){
+
+}
